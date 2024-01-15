2ad51c9a810bfbee5fd71898bafb9b44f7f6d6c2
TaskCompletes <147698186+TaskCompletes@users.noreply.github.com>
2024-01-15 14:34:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+from main import prerequest
+import datetime
+
+app=Flask(__name__,template_folder='template')
+
+current_start_time = None
+current_end_time = None
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/set_detection_time', methods=['POST'])
+def set_detection_time():
+    global current_start_time, current_end_time
+    message = prerequest()
+    return "Time set successfully"
+
+if __name__ == '__main__':
+    app.run(debug=True)
